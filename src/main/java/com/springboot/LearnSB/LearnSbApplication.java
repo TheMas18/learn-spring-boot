47a631f09ab39f485fa653ed98b824c52597e718
TheMas18 <mayureshsamant05@gmail.com>
2024-01-19 11:32:30 +0530

    Created First Spring Boot Project

---
+++ src/main/java/com/springboot/LearnSB/LearnSbApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.LearnSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSbApplication.class, args);
+	}
+
+}
