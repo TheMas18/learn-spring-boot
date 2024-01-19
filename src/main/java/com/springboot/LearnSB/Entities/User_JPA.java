8840b9455027dad9e7169c79e2cf793852430662
TheMas18 <mayureshsamant05@gmail.com>
2024-01-19 13:29:01 +0530

    Database Configuration

--- src/main/java/com/springboot/LearnSB/Entities/User_JPA.java
+++ src/main/java/com/springboot/LearnSB/Entities/User_JPA.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "User")
 public class User_JPA {
 
 	@Id
--- src/main/java/com/springboot/LearnSB/LearnSbApplication.java
+++ src/main/java/com/springboot/LearnSB/LearnSbApplication.java
@@ -2,12 +2,26 @@ package com.springboot.LearnSB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.springboot.LearnSB.Dao.UserRepository_JPA;
+import com.springboot.LearnSB.Entities.User_JPA;
 
 @SpringBootApplication
 public class LearnSbApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSbApplication.class, args);
+	ApplicationContext context=	SpringApplication.run(LearnSbApplication.class, args);
+	
+	UserRepository_JPA userRepository = context.getBean(UserRepository_JPA.class);
+	User_JPA user= new User_JPA();
+	user.setName("Mas");
+	user.setCity("Navi Mumbai");
+	user.setStatus("Full Stack Developer");
+	
+	User_JPA u = userRepository.save(user);
+	System.out.println(u);
 	}
 
 }
